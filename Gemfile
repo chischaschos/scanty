3ef6cc41811f9c54827525b1997a092355f64b67
chischaschos <larin.s931@gmail.com>
2012-12-28 20:25:32 -0600

    Updating code base
    * Not to vendorize gems
    * Remove tabs in files
    * Update Gemfile with previously vendorized gems
    * Use latest sequel versionm, hence update the way the posts table us
      recreated

--- Gemfile
+++ Gemfile
@@ -1,3 +1,12 @@
 source :gemcutter
+
+gem 'sequel'
+gem "maruku", "~> 0.6.1"
+gem "syntax", "~> 1.0.0"
 gem 'sinatra'
 gem 'sqlite3'
+gem 'debugger'
+
+group :test do
+  gem 'rspec'
+end
--- spec/base.rb
+++ spec/base.rb
@@ -1,15 +1,16 @@
 require 'rubygems'
-require 'spec'
-require 'sequel'
+require 'bundler'
 
-Sequel.sqlite
+Bundler.require :default, :test
+
+DB = Sequel.sqlite
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
 require 'post'
 
 require 'ostruct'
 Blog = OpenStruct.new(
-	:title => 'My blog',
-	:author => 'Anonymous Coward',
-	:url_base => 'http://blog.example.com/'
+  :title => 'My blog',
+  :author => 'Anonymous Coward',
+  :url_base => 'http://blog.example.com/'
 )
