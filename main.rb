7ea904849cc11d0b5b8693654a4236abdbe22097
chischaschos <larin.s931@gmail.com>
2012-12-29 11:39:37 -0600

    Convert remaining views to haml

--- main.rb
+++ main.rb
@@ -25,6 +25,7 @@ helpers do
   end
 end
 
+set :haml, format: :html5
 layout 'layout'
 
 ### Public
@@ -38,7 +39,7 @@ get '/past/:year/:month/:day/:slug/' do
   post = Post.filter(:slug => params[:slug]).first
   stop [ 404, "Page not found" ] unless post
   @title = post.title
-  erb :post, :locals => { :post => post }
+  haml :post, :locals => { :post => post }
 end
 
 get '/past/:year/:month/:day/:slug' do
@@ -48,14 +49,14 @@ end
 get '/past' do
   posts = Post.reverse_order(:created_at)
   @title = "Archive"
-  erb :archive, :locals => { :posts => posts }
+  haml :archive, :locals => { :posts => posts }
 end
 
 get '/past/tags/:tag' do
   tag = params[:tag]
   posts = Post.filter(:tags.like("%#{tag}%")).reverse_order(:created_at).limit(30)
   @title = "Posts tagged #{tag}"
-  erb :tagged, :locals => { :posts => posts, :tag => tag }
+  haml :tagged, :locals => { :posts => posts, :tag => tag }
 end
 
 get '/feed' do
@@ -71,7 +72,7 @@ end
 ### Admin
 
 get '/auth' do
-  erb :auth
+  haml :auth
 end
 
 post '/auth' do
@@ -81,7 +82,7 @@ end
 
 get '/posts/new' do
   auth
-  erb :edit, :locals => { :post => Post.new, :url => '/posts' }
+  haml :edit, :locals => { :post => Post.new, :url => '/posts' }
 end
 
 post '/posts' do
@@ -95,7 +96,7 @@ get '/past/:year/:month/:day/:slug/edit' do
   auth
   post = Post.filter(:slug => params[:slug]).first
   stop [ 404, "Page not found" ] unless post
-  erb :edit, :locals => { :post => post, :url => post.url }
+  haml :edit, :locals => { :post => post, :url => post.url }
 end
 
 post '/past/:year/:month/:day/:slug/' do
