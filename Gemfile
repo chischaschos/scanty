e75e67e27d28f41963dbe71d522e35d80520ff82
chischaschos <larin.s931@gmail.com>
2015-06-12 11:39:56 -0500

    - Fix issue causing assets to be seen as a git submodule - Upgrade gems and ruby

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
-ruby '2.1.2'
+
+ruby '2.2.2'
 
 gem 'capistrano', '~> 3.1.0'
 gem 'capistrano-bundler', '~> 1.1.2'
---
+++ assets/js/application.js
@@ -0,0 +1,5 @@
+//= require 'jquery'
+//= require 'js/vendor/fastclick'
+//= require 'js/foundation'
+
+$(function(){ $(document).foundation(); })
